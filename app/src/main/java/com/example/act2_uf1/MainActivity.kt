2549b9b963d282555a333e00dd9a9c0602e24b56
Tarda <tarda@tarda>
2024-10-10 19:59:23 +0200

    activitat 3

--- app/src/main/java/com/example/act2_uf1/MainActivity.kt
+++ app/src/main/java/com/example/act2_uf1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.act2_uf1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +14,19 @@ class MainActivity : AppCompatActivity() {
 
         val textViewHola: TextView = findViewById(R.id.textViewHola)
         val textViewMundo: TextView = findViewById(R.id.textViewMundo)
+        val color = resources.getColor(R.color.colorfondo, theme)
 
         textViewHola.text = "Hola"
         textViewMundo.text = "Món"
+
+        val main = findViewById<ConstraintLayout>(R.id.main)
+        val button = findViewById<Button>(R.id.button1)
+
+        button.setOnClickListener{
+            main.setBackgroundColor(color)
+        }
+
+
+
     }
 }
